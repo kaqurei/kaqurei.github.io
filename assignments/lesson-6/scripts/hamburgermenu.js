47af5c64529e85574a0e4bf9e70843f749677314
kaqurei <kaqurei@byui.edu>
2019-02-18 12:42:36 -0700

    Sync

--- assignments/lesson-6/scripts/hamburgermenu.js
+++ assignments/lesson-6/scripts/hamburgermenu.js
@@ -1,4 +1,13 @@
-        function hamburgerMenu(){
+function hamburgerMenu(){
+
+   var button = document.getElementById("menu-link");
+    var count = +1;
+
+  if (count % 2 == 1) {
     document.getElementById('menu').style.display = "block";
-    document.getElementById('menu').padding = "opx 0px 0px 0px";
-        }
+    document.getElementById('menu').padding = "0px 0px 0px 0px";
+}
+ else {
+    document.getElementById('menu').style.display = "none";
+}
+}
