816d3a67bb90d7e3dca352c654c830b71e7b7a3a
kaqurei <kaqurei@byui.edu>
2019-04-11 00:19:53 -0600

    Sync

--- assignments/term-project/js/hamburgermenu.js
+++
@@ -1,5 +0,0 @@
-//* Function for responsive hamburger menu on small display
-
-function hamburgerMenu(){
-	document.getElementsByClassName("menu")[0].classList.toggle("responsive");
-}
--- assignments/term-project/js/main-script.js
+++ assignments/term-project/js/main-script.js
@@ -1,3 +1,13 @@
+//* HAMBURGER MENU 
+//* Function for responsive hamburger menu on small display
+
+function hamburgerMenu(){
+	document.getElementsByClassName("menu")[0].classList.toggle("responsive");
+}
+
+//* SLIDES
+//* Function for operating slideshow on front page
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
